a4ff518b7b1387efa9e7eaa1e827cd1ea1770a32
sheroa <522libei@163.com>
2015-08-27 19:17:06 +0800

    头部的下拉js，登录页面修改

--- modules/common/util/navbar.js
+++ modules/common/util/navbar.js
@@ -6,25 +6,41 @@
 var $ = require('jquery');
 
 var navbar = {
-	init:function(index){
+	init: function(index) {
 
 		this.setMenuCurClass(index);
 		this.head_nav_hover();
 
 	},
-	setMenuCurClass:function(curIndex){
+	setMenuCurClass: function(curIndex) {
 		$("#nav").children('a').removeClass('selected').eq(curIndex).addClass("selected");
 	},
-	head_nav_hover:function(){
-		if(typeof($(this).attr("id"))=="undefined"){
-			return
-		}else{
-			var thisId=$(this).attr("id");
-			var subList=$(".'+thisId+'");
-			$(subList).addClass("show");
-		}
+	head_nav_hover: function() {
+		$("#nav").children('a').hover(function() {
+			if (!(typeof($(this).attr("id")) == "undefined")) {
+				var thisId = $(this).attr("id");
+				var subList = $("." + thisId);
+				$(subList).show();
+				$(this).addClass("selected");
+			} 
+		}, function() {
+			var thisId = $(this).attr("id");
+			var subList = $("." + thisId);
+			$(subList).hide();
+			$(this).removeClass("selected");
+		})
+		$("#nav").children('ul').hover(function() {
+			var thisClass = $(this).attr("class");
+			var aLink = $("#" + thisClass);
+			$(this).show();
+			$(aLink).addClass("selected");
+		}, function() {
+			var thisClass = $(this).attr("class");
+			var aLink = $("#" + thisClass);
+			$(this).hide();
+			$(aLink).removeClass("selected");
+		})
 	}
 }
 
 module.exports = navbar;
-
