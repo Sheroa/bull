5921737d486029e5dbc04dfb4549e13046667f69
zy_520 <zhaoyongbtbu@163.com>
2015-08-31 15:42:05 +0800

    更新个人中心相关代码

--- modules/pages/my/person_center.js
+++ modules/pages/my/person_center.js
@@ -3,8 +3,36 @@
  * @author  : ZY
  */
 
-var sidebar = require("util/sidebar"),
-	navBar = require("util/navbar");
+var navBar = require("util/navbar"),
+	sidebar = require("util/sidebar"),
+	$      = require('jquery');
 
-navBar.init(index);
-sidebar.init();
+require('util/extend_fn');
+
+var index_page = {
+	init:function(){
+		this.nav_bar(index);
+		this.side_bar();
+		this.tab_switch();
+	},
+	nav_bar:function(index){
+		navBar.init(index);
+	},
+	side_bar:function(){
+		sidebar.init();
+	},
+	tab_switch:function(){
+		$(".horizon").each(function(){
+			var _this = $(this);
+			_this.tabSwitch({
+				navObj:'.a-tab',
+				className:'.front-area',
+				curSel:'selected',
+				selectorIndex:'.a-tab',
+				eventName:'mouseenter'
+			});
+		});
+	}
+}
+
+index_page.init();
