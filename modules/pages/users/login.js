1ff65a51b3fde8b54d3d778bf99bcf524ca5b978
赵永 <zhaoyongbtbu@163.com>
2015-09-02 14:52:14 +0800

    修改注册页面代码

--- modules/pages/users/login.js
+++ modules/pages/users/login.js
@@ -17,27 +17,31 @@ var login = {
 			var login_check = self.valid_check();
 			if(login_check){
 				//显示错误信息
+				$(".error-msg").html(login_check);
 				return false;
 			}
 		});
 	},
 	valid_check:function(){
-		var user_name = $("#userName"),
-			user_pwd  = $("#userPass");
+		var phone_num = $("#phone_num"),
+			user_pwd  = $("#user_pwd");
 
 
 		//检测手机号码
-		if(!(user_name && user_name.val() != '')){
+		if(!(phone_num && phone_num.val() != '')){
 			return "请输入手机号码";
 		}
 
-		if(!K.is_phone($.trim(user_name.val()))){
+		if(!K.is_phone($.trim(phone_num.val()))){
 			return "手机号码格式不正确，请重新输入";
 		}
 		
+		//校验登陆密码
 		if(!(user_pwd && user_pwd.val() != '')){
 			return "请输入登陆密码";
 		}
+
+
 	}
 }
 
