6acf207427e809ec406815e3f3a50d0cd17794d9
赵永 <zhaoyongbtbu@163.com>
2015-09-08 13:57:10 +0800

    修改一些Bug

--- modules/pages/my/bank_card.js
+++ modules/pages/my/bank_card.js
@@ -28,17 +28,33 @@
  		//页面渲染
  		api.call('/api/user/getIdentityInfoByUser.do',{},function(data){
  			var bank_card_num = data.result.bankCardNo,
- 				container     = $(".add-bank");
+ 				container     = $(".add-bank"),
+ 				container_binded = $('#bank_info');
  			if(!bank_card_num){
  				//用户没有绑定银行卡
  				container.show();
  			}else{
  				//用户绑定银行卡
+ 				var obj = data.result,
+ 					info_pad = container_binded.find(".border-pad"),
+ 					buf = [];
+ 				buf.push('<p class="bank-name">'+data.result.bankCode+'</p>');
+ 				buf.push('<p class="card-kind">储蓄卡</p>');
+ 				buf.push('<p class="card-owner">户名：'+K.name_map(data.result.userName)+'</p>');
+ 				if(data.result.bindCard){
+ 					buf.push('<p class="card-bound"><i>已绑定</i><em>'+K.bank_card_map(data.result.bankCardNo)+'</em></p>');	
+ 					container_binded.append('<p class="sub-text">单笔限额20万，单日限额200万。<br>实际请参考您的银行限额设置。</p>');
+ 				}else{
+ 					buf.push('<p class="card-num">'+K.bank_card_map(data.result.bankCardNo)+'</p>');
+ 					container_binded.append('<a href="javascript:void(0);" class="alter">修改</a>');	
+ 				}
+ 								
+ 				info_pad.html(buf.join(""));
 
+ 				container_binded.show();
  			}
  			_self.event_handler();
  		});
-
 
 		//银行列表渲染
 		api.call('/api/payment/queryBankList',{},function(data){
@@ -74,8 +90,7 @@
  		
  		var self = this;
 
- 		//添加银行卡
- 		$(".add-bank").on("click",function(){
+ 		function bind_func(){
  			var _this = $(this);
  				
 
@@ -145,7 +160,13 @@
  			    	});
  			    }
  			});
- 		});
+ 		}
+
+ 		//添加银行卡
+ 		$(".add-bank").on("click",bind_func);
+
+ 		$(".alter").on("click",bind_func);
+
  	}
  }
 
